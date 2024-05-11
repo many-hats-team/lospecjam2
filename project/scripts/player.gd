3ba66232c8b062fba0b292019b845441a0072f99
Kris Scott <kscott91@gmail.com>
2024-05-11 12:35:00 -0500

    Add player sprite

--- project/scripts/player.gd
+++ project/scripts/player.gd
@@ -3,15 +3,29 @@ extends CharacterBody3D
 
 const BulletScene := preload("res://scenes/bullet.tscn")
 
-
 const MOVE_INPUT_SCALE := Vector2(3.0, 3.0)
 const MOVE_LERP_BASE := 0.3
 const MOVE_LERP_SCALE := 8.0
 
+const SPRITE_HORIZONTAL_DEADZONE := 1.0
+
+
+@export var image_center: Image
+@export var image_left: Image
+@export var image_right: Image
+
+@onready var texture_center := ImageTexture.create_from_image(image_center)
+@onready var texture_left := ImageTexture.create_from_image(image_left)
+@onready var texture_right := ImageTexture.create_from_image(image_right)
+
+@onready var sprite := %Sprite as MySprite
+
 
 func _ready() -> void:
+	assert(sprite)
 	HitBox.new(self, hit)
 	mgmt.player = self
+	sprite.texture = texture_center
 
 
 func get_input_vector() -> Vector2:
@@ -23,9 +37,16 @@ func _physics_process(delta: float) -> void:
 	var weight = pow(MOVE_LERP_BASE, delta * MOVE_LERP_SCALE)
 	velocity.x = lerpf(target_velocity.x, velocity.x, weight)
 	velocity.z = lerpf(target_velocity.y, velocity.z, weight)
+
+	if velocity.x > SPRITE_HORIZONTAL_DEADZONE:
+		sprite.texture = texture_right
+	elif velocity.x < -SPRITE_HORIZONTAL_DEADZONE:
+		sprite.texture = texture_left
+	else:
+		sprite.texture = texture_center
+
 	move_and_slide()
 
 
 func hit() -> void:
-	print("hit")
-
+	sprite.hit_flash()
--- project/scripts/sprite.gd
+++ project/scripts/sprite.gd
@@ -1,9 +1,41 @@
+@tool
 class_name MySprite
 extends Sprite3D
 
 const FLASH_DURATION_FRAMES := 2
 
+@export var frame_duration_s := 0.1:
+	set(value):
+		frame_duration_s = value
+		if timer:
+			timer.wait_time = value
+
+@onready var timer := %Timer as Timer
+
 var _is_flashing := false
+
+func _ready() -> void:
+	assert(timer)
+	frame_duration_s = frame_duration_s
+	timer.timeout.connect(_on_timer_timeout)
+
+	_apply_texture()
+	texture_changed.connect(_apply_texture)
+	frame_changed.connect(_apply_texture)
+
+
+func _apply_texture():
+	if not texture:
+		return
+
+	var shader_material : ShaderMaterial = material_override
+	shader_material.set_shader_parameter("texture_albedo", texture)
+	shader_material.set_shader_parameter("uv1_offset", Vector3(
+		posmod(frame, vframes) * (texture.get_width() / hframes),
+		(frame / hframes) * (texture.get_height() / vframes),
+		0.0
+	))
+
 
 func hit_flash() -> void:
 	if not _is_flashing:
@@ -14,3 +46,7 @@ func hit_flash() -> void:
 		shader_material.set_shader_parameter("flash", false)
 		await mgmt.wait_phys_frames(FLASH_DURATION_FRAMES)
 		_is_flashing = false
+
+
+func _on_timer_timeout() -> void:
+	frame = posmod(frame + 1, vframes * hframes)
