4918071bc3af807ef3d640a80912f8be51d45dd3
Kris Scott <kscott91@gmail.com>
2024-05-16 22:51:07 -0500

    Add parallax background

--- project/scripts/camera_follow.gd
+++ project/scripts/camera_follow.gd
@@ -2,14 +2,23 @@ extends Camera3D
 
 @export var enabled := true
 @export var target_path: NodePath
+@export var parallax_path: NodePath
 
 @onready var target := get_node(target_path) as Node3D
+@onready var bg := get_node(parallax_path) as ParallaxBackground
+
+var level_scroll := 0.0
 
 func _ready() -> void:
 	assert(target)
+	assert(bg)
 
 
 func _process(delta: float) -> void:
+	level_scroll += delta
+
 	if enabled:
 		var weight := pow(0.3, delta * 10.0)
 		global_position.x = lerpf(target.global_position.x, global_position.x, weight)
+
+		bg.scroll_offset = Vector2(-global_position.x, -level_scroll)
