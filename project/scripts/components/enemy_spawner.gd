1a7c1fa1bab27a7e7adcdc594cf8d33a3645374e
Kris Scott <kscott91@gmail.com>
2024-05-31 10:26:37 -0500

    Separate enemy scene into car and biker

--- project/scripts/components/enemy_spawner.gd
+++ project/scripts/components/enemy_spawner.gd
@@ -1,26 +1,52 @@
-extends Timer
+class_name EnemySpawner
+extends Marker3D
 
-const EnemyScene := preload("res://scenes/objects/enemy.tscn")
+const CarScene := preload("res://scenes/objects/car.tscn")
+const BikerScene := preload("res://scenes/objects/biker.tscn")
 
+enum Kind {
+	CAR,
+	BIKER,
+}
+
+@export var kind: Kind
 
 @export var speed: float
 @export var offset: float
+@export var wait_min: float
+@export var wait_max: float
 
-@onready var source_node := get_parent() as Node3D
 @onready var rng := RandomNumberGenerator.new()
 
+var timer: Timer
+
 func _ready() -> void:
-	assert(source_node)
-	timeout.connect(_on_timeout)
-	mgmt.boss_died.connect(stop)
+	timer = Timer.new()
+	timer.process_callback = Timer.TIMER_PROCESS_PHYSICS
+	timer.autostart = true
+	timer.timeout.connect(_on_timeout)
+	mgmt.boss_died.connect(timer.stop)
+	_pick_new_wait_time()
+	add_child(timer)
+
+
+func _pick_new_wait_time() -> void:
+	timer.wait_time = rng.randf_range(wait_min, wait_max)
 
 
 func _on_timeout() -> void:
+	_pick_new_wait_time()
+
 	var world := mgmt.get_world()
 	if world:
-		var enemy := EnemyScene.instantiate()
+		var enemy: Enemy
+		match kind:
+			Kind.CAR:
+				enemy = CarScene.instantiate() as Enemy
+			Kind.BIKER:
+				enemy = BikerScene.instantiate() as Enemy
 		world.add_child(enemy)
 		enemy.setup(
-			source_node.global_position + Vector3(rng.randf_range(-offset, offset), 0, 0),
+			global_position + Vector3(rng.randf_range(-offset, offset), 0, 0),
 			Vector3.BACK * speed
 		)
--- project/scripts/objects/enemy.gd
+++ project/scripts/objects/enemy.gd
@@ -1,3 +1,4 @@
+class_name Enemy
 extends CharacterBody3D
 
 
